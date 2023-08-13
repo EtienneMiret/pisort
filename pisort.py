8d61560d1ee82e778a6ef27dbb5139f19ae1810c
Étienne Miret <etienne.miret@ens-lyon.org>
2023-08-13 15:19:29 +0200

    List Exif data of files from current directory

--- pisort.py
+++ pisort.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+from pathlib import Path
+from PIL import Image, UnidentifiedImageError, ExifTags
+
+
+def list_pictures(directory: Path) -> None:
+    for file in directory.iterdir():
+        if not file.is_file():
+            continue
+        try:
+            exif = Image.open(file).getexif()
+            print(f'{file.name} has EXIF:')
+            for k, v in exif.items():
+                print(f'  {ExifTags.TAGS[k]}: {v}')
+        except UnidentifiedImageError:
+            pass
+
+
+if __name__ == "__main__":
+    list_pictures(Path("."))
