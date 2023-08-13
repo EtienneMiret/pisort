4363dce9d1445d41fb173f1e561a1708534d7a64
Étienne Miret <etienne.miret@ens-lyon.org>
2023-08-13 15:35:17 +0200

    Create Picture class

--- pisort.py
+++ pisort.py
@@ -1,21 +1,39 @@
 from pathlib import Path
+from typing import Optional, TextIO
+
 from PIL import Image, UnidentifiedImageError, ExifTags
 
 
-def list_pictures(directory: Path) -> None:
+class Picture:
+
+    def __init__(self, path: Path):
+        self.path = path
+        with Image.open(path) as img:
+            self.exif = img.getexif()
+
+    def __str__(self) -> str:
+        return self.path.name
+
+    def print(self, file: Optional[TextIO] = None) -> None:
+        print(f'{self.path.name}:')
+        for k, v in self.exif.items():
+            print(f'  {ExifTags.TAGS[k]}: {v}', file=file)
+        for k, v in self.exif.get_ifd(ExifTags.IFD.Exif).items():
+            print(f'  {ExifTags.TAGS[k]}: {v}', file=file)
+
+
+def list_pictures(directory: Path) -> list[Picture]:
+    result = []
     for file in directory.iterdir():
         if not file.is_file():
             continue
         try:
-            exif = Image.open(file).getexif()
-            print(f'{file.name} has EXIF:')
-            for k, v in exif.items():
-                print(f'  {ExifTags.TAGS[k]}: {v}')
-            for k, v in exif.get_ifd(ExifTags.IFD.Exif).items():
-                print(f'  {ExifTags.TAGS[k]}: {v}')
+            result.append(Picture(file))
         except UnidentifiedImageError:
             pass
+    return result
 
 
 if __name__ == "__main__":
-    list_pictures(Path("."))
+    for image in list_pictures(Path(".")):
+        image.print()
