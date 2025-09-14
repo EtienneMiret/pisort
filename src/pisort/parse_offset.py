318ff6fb73af1f758c29acc9de1d6ef4418af49e
Étienne Miret <etienne.miret@ens-lyon.org>
2025-09-14 18:06:14 +0200

    Use tzlocal to get local TZ

--- src/pisort/parse_offset.py
+++ src/pisort/parse_offset.py
@@ -1,13 +1,15 @@
 import datetime
 import re
 
-exif_offset_re = re.compile("([+-])(\\d\\d):(\\d\\d)", re.ASCII)
+import tzlocal
 
+
+exif_offset_re = re.compile("([+-])(\\d\\d):(\\d\\d)", re.ASCII)
 
 def parse_offset(string: str) -> datetime.tzinfo:
     match = exif_offset_re.fullmatch(string)
     if not match:
-        return datetime.datetime.now().astimezone().tzinfo
+        return tzlocal.get_localzone()
     delta = datetime.timedelta(
         hours=int(match.group(2)),
         minutes=int(match.group(3)),
